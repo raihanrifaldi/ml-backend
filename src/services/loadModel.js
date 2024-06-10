4dde7f3f3bd04354c8da12bb2851bebc7f62800d
raihanrifaldi <raihanrifaldiii@gmail.com>
2024-06-10 19:07:39 +0000

    upload file

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,7 +1,8 @@
 const tf = require("@tensorflow/tfjs-node");
 
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+    const model="https://storage.googleapis.com/cancer-prediction/model.json"
+    return tf.loadGraphModel(model);
 }
 
 module.exports = loadModel;
